0c21cc85552461a7f5d36150e5e82f881329c440
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-10-15 13:09:50 +0200

    lkujblkubhulug

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 using System.Data.SqlTypes;
+using System.Xml.Serialization;
 
 bool success = false;
 int ageNum = 0;
 int cash = 100;
 bool bad = false;
+int timesTimes = 0;
 
 while (success != true)
 {
@@ -35,10 +37,60 @@ else if (ageNum <= 17)
     }
 }
 
-Console.WriteLine("Vad  vill du köpa?");
-while (cash >= 0)
-Console.WriteLine($"Du har {cash}pengar");
-Console.WriteLine("1. hus(30) 2. bord(25) 3. låda(15)");
+
+while (cash > 0)
+{
+    Console.WriteLine("Vad  vill du köpa?");
+    Console.WriteLine($"Du har {cash}pengar");
+    Console.WriteLine("hus(30) bord(20) låda(10)");
 
 
+    string choice = Console.ReadLine();
+
+    if (choice.ToLower() == "hus")
+    {
+        Console.WriteLine("Hur många vill du köpa?");
+        string times = Console.ReadLine();
+        int.TryParse(times, out timesTimes);
+        if (30 * timesTimes <= cash)
+        {
+            cash -= 30 * timesTimes;
+        }
+        else if (30 * timesTimes > cash)
+        {
+            Console.WriteLine("DU ÄR FÖR FATTIG DIN JÄVLA BONDE");
+        }
+        
+
+    }
+    else if (choice.ToLower() == "bord")
+    {
+         Console.WriteLine("Hur många vill du köpa?");
+        string times = Console.ReadLine();
+        int.TryParse(times, out timesTimes);
+        if (20 * timesTimes <= cash)
+        {
+            cash -= 20 * timesTimes;
+        }
+        else if (20 * timesTimes > cash)
+        {
+            Console.WriteLine("DU ÄR FÖR FATTIG DIN JÄVLA BONDE");
+        }
+    }
+    else if (choice.ToLower() == "låda")
+    {
+         Console.WriteLine("Hur många vill du köpa?");
+        string times = Console.ReadLine();
+        int.TryParse(times, out timesTimes);
+        if (10 * timesTimes <= cash)
+        {
+            cash -= 10 * timesTimes;
+        }
+        else if (10 * timesTimes > cash)
+        {
+            Console.WriteLine("DU ÄR FÖR FATTIG DIN JÄVLA BONDE");
+        }
+    }
+}
+Console.WriteLine("Tack för att du handlade");
 Console.ReadLine();
